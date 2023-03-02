86fbfb4fec07702620a3d40ca6c874f0bc5cb850
bernalqz <bernalqz@gmail.com>
2023-03-02 13:35:28 -0600

    Select en el index

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -10,15 +10,21 @@
 
     <h4>Ingreso de jugadores</h4>
 
-    <div class="container">
-<form action="public/php/registUser.php">
+    <div>
+<form action="public/php/registGamer.php">
 <label for=""></label>
 <input type="text" placeholder="Nombre"></input><br><br>
 <input type="text" placeholder="Apellido"></input><br><br>
 <input type="number" placeholder="Numéro a apostar" max="100"></input><br><br>
 <input type="number" placeholder="Dinero a apostar" max="100"></input><br><br>
-<input type="submit" value="Registrar"></input>
+<label >Tipo de Tiempos:</label><br><br>
+    <select >
+        <option >Ticos</option>
+        <option >Nica</option>
+        <option >Panameños</option>
+    </select><br><br><br>
 
+<input type="submit" value="Registrar"></input>
 
 </form>
     </div>
