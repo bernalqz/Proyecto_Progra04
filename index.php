a8435e4550d41564b280552a4186762b4091473e
bernalqz <bernalqz@gmail.com>
2023-03-02 12:57:56 -0600

    Form

--- index.php
+++ index.php
@@ -7,6 +7,21 @@
     <title>Proyecto de Progra 4</title>
     
     <h2>Juego de Tiempos</h2>
+
+    <h4>Ingreso de jugadores</h4>
+
+    <div class="container">
+<form action="public/php/registUser.php">
+<label for=""></label>
+<input type="text" placeholder="Nombre"></input><br><br>
+<input type="text" placeholder="Apellido"></input><br><br>
+<input type="number" placeholder="Numéro a apostar" max="100"></input><br><br>
+<input type="number" placeholder="Dinero a apostar" max="100"></input><br><br>
+<input type="submit" value="Registrar"></input>
+
+
+</form>
+    </div>
 </head>
 <body>
     
