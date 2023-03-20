4a6e5d77fca2da826bc1c5b97e27fbc3723cbf38
bernalqz <bernalqz@gmail.com>
2023-03-20 17:42:50 -0600

    Lista Jugadores

--- app/views/lista-de-jugadores.php
+++ app/views/lista-de-jugadores.php
@@ -1,7 +1,39 @@
 <?php include "header.php"?>
 
+<br>
+<br>
+<br>
+<table class="content-table">
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Nombre</th>
+            <th>Apodo</th>
+            <th>Numeros Ganadores</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Juancho</td>
+            <td>Popo</td>
+            <td>0</td>
+          </tr>
+          <tr class="active-row">
+            <td>2</td>
+            <td>Pedro</td>
+            <td>Manudo</td>
+            <td>1</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>Nick</td>
+            <td>Tristón</td>
+            <td>3</td>
+          </tr>
+        </tbody>
+      </table>
 
-Lista
 
 
 <?php include "footer.php"?>
