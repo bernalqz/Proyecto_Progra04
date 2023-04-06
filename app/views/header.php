4286654eeb21d39229705a4b26ff24f034eeea08
luiscode9 <luisbossa9@gmail.com>
2023-04-06 11:44:36 -0600

    numero random

--- app/views/header.php
+++ app/views/header.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.1">
     <script src="https://kit.fontawesome.com/76ceaa8f89.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="../../assets/css/style.css?ver=1.5">
+    <link rel="stylesheet" href="../../assets/css/style.css?ver=1.0">
     
     <title>El Juego de los tiempos</title>
 </head>
--- app/views/panel-de-juego.php
+++ app/views/panel-de-juego.php
@@ -1,14 +1,21 @@
 <?php include "header.php"?>
 
 <div class="title-head">  
-<h1>Números ganadores</h1>    
+<h1>Número ganador</h1>    
 </div> 
 
 <div class="ventanas">
+
+<div>
+  <h3 class="random_ball" id=ball>0</h3><br>
+</div>
+
+<button class="btn" id="button-run">Sortear</button><br><br>
+
+
 <form action="../../assets/php/panel_de_juego.php">
 <label for="">Número ganador:</label>
-
-<input type="number" min="0" max="100" required placeholder=""></input><br><br>
+<input type="number" min="0" max="100" required></input><br><br>
 <label for="">Sorteo:</label>
 <select name="" id="">
 <option value="">A Todas</option>  
@@ -21,10 +28,27 @@
 <label for="">Lista de Ganadores:</label>
 <br><br>  <br><br> 
 <label for=""> Espacio para mostrar Lista de Ganadores</label> 
-<br><br>  <br><br> 
+<br><br> 
 <input type="submit" class="btn" value="Ganadores"></input>
-
 </form>
 </div>
+
+<script>
+
+  const my_ball = document.getElementById("ball");
+
+  const button_run = document.getElementById("button-run");
+
+  const random_num_generator = () => {
+     
+    const random_num = Math.floor(Math.random() * 100 + 1);
+    ball.textContent = random_num;
+
+  };
+
+  button_run.addEventListener('click', random_num_generator);
+  random_num_generator();
+
+</script>
 
 <?php include "footer.php"?>
